d5d7c76f8b63d8e9f92b87f956aac60422a397e6
Ana Jalba <ana.jalba13@gmail.com>
2018-08-22 12:24:44 +0100

    Add tests for DataExtractor

--- src/test/java/com/hotels/bdp/circustrain/bigquery/extraction/service/DataExtractorTest.java
+++ src/test/java/com/hotels/bdp/circustrain/bigquery/extraction/service/DataExtractorTest.java
@@ -15,15 +15,19 @@
  */
 package com.hotels.bdp.circustrain.bigquery.extraction.service;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Queue;
+import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -36,7 +40,6 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatcher;
 import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.cloud.RetryOption;
@@ -45,8 +48,10 @@ import com.google.cloud.bigquery.Job;
 import com.google.cloud.bigquery.JobStatus;
 import com.google.cloud.bigquery.Table;
 import com.google.cloud.bigquery.TableId;
+import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
 
 import com.hotels.bdp.circustrain.api.CircusTrainException;
 import com.hotels.bdp.circustrain.bigquery.extraction.container.ExtractionContainer;
@@ -58,29 +63,34 @@ public class DataExtractorTest {
 
   public @Rule ExpectedException expectedException = ExpectedException.none();
 
-  private DataExtractor extractor;
   private @Mock Storage storage;
   private @Mock Table table;
   private @Mock Job job;
   private @Mock JobStatus jobStatus;
   private @Mock Future future;
   private @Mock ExecutorService executorService;
-  private final Queue<ExtractionContainer> queue = new ConcurrentLinkedQueue<>();
+  private DataExtractor extractor;
+  private final ExtractionUri data = new ExtractionUri();
+  private final TableId tableId = TableId.of("dataset", "table");
+  private ExtractionContainer extractionContainer;
 
   @Before
   public void init() throws ExecutionException, InterruptedException {
     initExecutor();
-    extractor = new DataExtractor(storage, queue);
+
+    extractor = new DataExtractor(storage);
+    extractionContainer = new ExtractionContainer(table, data, PostExtractionAction.RETAIN);
+
+    when(table.getTableId()).thenReturn(tableId);
+    when(table.extract(anyString(), anyString())).thenReturn(job);
+    when(job.waitFor(Matchers.<RetryOption> anyVararg())).thenReturn(job);
+    when(job.getStatus()).thenReturn(jobStatus);
   }
 
-  private void initExecutor() {
-    try {
-      when(future.get()).thenReturn(null);
-    } catch (InterruptedException | ExecutionException e) {
-      throw new RuntimeException(e);
-    }
-    when(executorService.submit(any(Callable.class))).thenReturn(future);
-    Mockito.when(executorService.submit(Matchers.argThat(new ArgumentMatcher<Callable>() {
+  private void initExecutor() throws InterruptedException, ExecutionException {
+    when(future.get()).thenReturn(null);
+    when(executorService.submit((Callable) anyObject())).thenReturn(future);
+    when(executorService.submit(Matchers.argThat(new ArgumentMatcher<Callable>() {
       @Override
       public boolean matches(Object argument) {
         Callable callable = (Callable) argument;
@@ -91,25 +101,19 @@ public class DataExtractorTest {
         }
         return true;
       }
-
     }))).thenReturn(future);
   }
 
   @Test
   public void extract() throws InterruptedException {
-    ExtractionUri data = new ExtractionUri();
-    TableId tableId = TableId.of("dataset", "table");
-    when(table.extract(anyString(), anyString())).thenReturn(job);
-    when(table.getTableId()).thenReturn(tableId);
-    when(job.waitFor(Matchers.<RetryOption> anyVararg())).thenReturn(job);
-    when(job.getStatus()).thenReturn(jobStatus);
     when(jobStatus.getError()).thenReturn(null);
 
-    extractor.add(new ExtractionContainer(table, data, PostExtractionAction.RETAIN));
-    extractor.extract(executorService);
+    extractor.add(extractionContainer);
+    List<ExtractionContainer> extracted = extractor.extract(executorService);
 
     verify(storage).create(any(BucketInfo.class));
     verify(table).extract(eq("csv"), eq(data.getUri()));
+    assertThat(extracted.size(), is(1));
   }
 
   @Test
@@ -117,13 +121,9 @@ public class DataExtractorTest {
     expectedException.expect(CircusTrainException.class);
     expectedException.expectMessage("job no longer exists");
 
-    ExtractionUri data = new ExtractionUri();
-    TableId tableId = TableId.of("dataset", "table");
-    when(table.getTableId()).thenReturn(tableId);
-    when(table.extract(anyString(), anyString())).thenReturn(job);
     when(job.waitFor(Matchers.<RetryOption> anyVararg())).thenReturn(null);
 
-    extractor.add(new ExtractionContainer(table, data, PostExtractionAction.RETAIN));
+    extractor.add(extractionContainer);
     extractor.extract(executorService);
   }
 
@@ -135,16 +135,48 @@ public class DataExtractorTest {
     expectedException.expectMessage(bigQueryError.getLocation());
     expectedException.expectMessage(bigQueryError.getMessage());
 
-    ExtractionUri data = new ExtractionUri();
-    TableId tableId = TableId.of("dataset", "table");
-    when(table.getTableId()).thenReturn(tableId);
-    when(table.extract(anyString(), anyString())).thenReturn(job);
-    when(job.waitFor(Matchers.<RetryOption> anyVararg())).thenReturn(job);
-    when(job.getStatus()).thenReturn(jobStatus);
-    when(jobStatus.getError()).thenReturn(new BigQueryError("reason", "getExtractedDataBaseLocation", "message"));
+    when(jobStatus.getError()).thenReturn(bigQueryError);
 
-    extractor.add(new ExtractionContainer(table, data, PostExtractionAction.RETAIN));
+    extractor.add(extractionContainer);
     extractor.extract(executorService);
+  }
+
+  @Test(expected = CircusTrainException.class)
+  public void extractInterruptedFuture() throws InterruptedException, ExecutionException {
+    when(future.get()).thenThrow(new InterruptedException());
+    extractor.add(extractionContainer);
+    extractor.extract(executorService);
+  }
+
+  @Test(expected = CircusTrainException.class)
+  public void extractFuturewithExecutionException() throws InterruptedException, ExecutionException {
+    when(future.get()).thenThrow(new ExecutionException(null));
+    extractor.add(extractionContainer);
+    extractor.extract(executorService);
+  }
+
+  @Test
+  public void extractWithNoArguments() {
+    List<ExtractionContainer> extracted = extractor.extract();
+    assertThat(extracted.size(), is(0));
+  }
+
+  @Test
+  public void extractWhenBucketAlreadyExists() {
+    when(storage.get(anyString())).thenReturn(mock(Bucket.class));
+    extractor.add(extractionContainer);
+    List<ExtractionContainer> extracted = extractor.extract(executorService);
+    verify(storage, never()).create((BucketInfo) anyObject());
+    assertThat(extracted.size(), is(1));
+  }
+
+  @Test
+  public void errorWhenCheckingIfBucketExists() {
+    when(storage.get(anyString())).thenThrow(new StorageException(0, "message"));
+    extractor.add(extractionContainer);
+    List<ExtractionContainer> extracted = extractor.extract(executorService);
+    verify(storage).create((BucketInfo) anyObject());
+    assertThat(extracted.size(), is(1));
   }
 
 }
