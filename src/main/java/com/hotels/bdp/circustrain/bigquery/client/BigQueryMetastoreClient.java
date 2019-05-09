daa790fa086a0a2dc7d6d428b32dd2bcd0b73cee
Adrian Woodhead <massdosage@gmail.com>
2019-05-09 18:43:30 +0100

    Upgrades to Circus Train and parent (#33)
    * Upgraded `circus-train-version` to 14.0.1 (was 13.0.0).
    * Upgraded `hotels-oss-parent` to 4.0.1 (was 2.3.3).

--- src/main/java/com/hotels/bdp/circustrain/bigquery/client/BigQueryMetastoreClient.java
+++ src/main/java/com/hotels/bdp/circustrain/bigquery/client/BigQueryMetastoreClient.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018 Expedia Inc.
+ * Copyright (C) 2018-2019 Expedia, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -124,11 +124,6 @@ class BigQueryMetastoreClient implements CloseableMetaStoreClient {
     this.tableServiceFactory = tableServiceFactory;
     this.cache = cache;
     this.schemaExtractor = schemaExtractor;
-  }
-
-  @Override
-  public boolean isOpen() {
-    return true;
   }
 
   @Override
